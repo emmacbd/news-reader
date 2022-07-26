9656d9f18d5f5d791810eeb0f80c47222b290a34
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-07-26 12:59:16 -0500

    Add routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,36 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchArticles } from '../../apiCalls';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import Header from './Header';
+import NavBar from './NavBar';
+import ArticleDetails from './ArticleDetails';
 import './App.css';
 
 
 const App = () => {
-const [allArticles, setArticles] = useState([])
-const [error, setError] = useState('')
+  const [allArticles, setArticles] = useState([])
+  const [error, setError] = useState('')
 
-useEffect(() => {
-  fetchArticles.getHomeArticles()
-    .then(data => setArticles(data.results))
-    .catch(error => setError(error.message))
-}, [])
-
-
-
-const searchByCategory = (topic) => {
-  useEffect((topic) => {
-    fetchArticles.getArticlesByCategory(topic)
+  useEffect(() => {
+    fetchArticles.getHomeArticles()
       .then(data => setArticles(data.results))
       .catch(error => setError(error.message))
-  })
-}
+  }, [])
+
+
+
+  const searchByCategory = (topic) => {
+    useEffect((topic) => {
+      fetchArticles.getArticlesByCategory(topic)
+        .then(data => setArticles(data.results))
+        .catch(error => setError(error.message))
+    }, [])
+  }
 
 
   return (
     <main className="App">
-      <NavBar /> 
-
+      <NavBar />
+      <Switch>
+        <Route exact path="/" newsDrop={allArticles} searchArticles={searchByCategory}>
+          {Dashboard}
+        </Route>
+        <Route exact path="">
+          {ArticleDetails}
+        </Route>
+      </Switch>
     </main>
   );
 }
